c83f340e51150ff2a4f279aa2781adc8460d9c13
FilipM995 <92673450+FilipM995@users.noreply.github.com>
2024-01-16 16:50:08 +0100

    Update eval.py

--- eval.py
+++ eval.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 import time
 
 
-def compute_metrics(test_set, seg_model, clf_model):
-    pr_auc_clf = tf.keras.metrics.AUC(curve='PR')
-    pr_auc_seg = tf.keras.metrics.AUC(curve='PR')
+def compute_metrics(test_set, seg_model, clf_model, curve='PR'):
+    pr_auc_clf = tf.keras.metrics.AUC(curve=curve)
+    pr_auc_seg = tf.keras.metrics.AUC(curve=curve)
     img_num = 0
 
     infer_time = 0
@@ -22,10 +22,11 @@ def compute_metrics(test_set, seg_model, clf_model):
         pr_auc_clf.update_state(lbl, Cp)
         pr_auc_seg.update_state(mask, Sh)
         img_num += img.shape[0]
-        print(img_num)
 
     return {
         'AP_clf': float(pr_auc_clf.result().numpy()),
         'AP_seg': float(pr_auc_seg.result().numpy()),
         'avg_infer_time': infer_time / img_num,
     }
+
+
